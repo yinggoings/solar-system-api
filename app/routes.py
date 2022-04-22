7ac8fff29b854d9ff8a06c85bb7e160326f9dbcd
Ying G <yingg@yings-air.lan>
2022-04-22 11:47:49 -0700

    complete wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, moons):
@@ -8,7 +8,7 @@ class Planet:
         self.moons = moons
 
 planets = [
-    Planet(1, "Mercury", "Mercury’s craters are named after famous artists, musicians and authors.", 0),
+    Planet(1, "Mercury", "Mercury's craters are named after famous artists, musicians and authors.", 0),
     Planet(2 , "Venus", "Venus is the hottest planet in the solar system.", 0),
     Planet(3, "Earth", "Is Hell. Is a prison for my weary bones to which I am condemned.", 1),
     Planet(4, "Mars", "There have been more missions to Mars than any other planet.", 2),
@@ -17,3 +17,17 @@ planets = [
     Planet(7, "Uranus", "Uranus has only been visited by a single spacecraft, Voyager 2", 27),
     Planet(8, "Neptune", "It takes like more than 4 hours for light to reach Neptune from the Sun.", 14)
 ]
+
+planets_bp = Blueprint("planets_bp",__name__,url_prefix="/planets")
+
+@planets_bp.route("",methods=["GET"])
+def get_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moons": planet.moons
+        })
+    return jsonify(planets_response)
