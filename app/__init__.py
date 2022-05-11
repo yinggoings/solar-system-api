44518e091a924d1b07e587303cf75c222c281d88
Amel <amelntamark@gmail.com>
2022-05-11 11:22:11 -0700

    ahhh

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jxkiopeppzhkfa:4fba5c6833078917e812eeaa982c6d7f869ed2c0f570935f0d31b3fe83dccca3@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d9qdee50ds4ser'
 
     db.init_app(app)
     migrate.init_app(app,db)
